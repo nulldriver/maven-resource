03a7cfc7acea63e687d287454293dbd90be2ec1b
patrickcrocker <patrick.crocker@gmail.com>
2017-03-22 19:40:06 -0500

    added nexus docker start script

---
+++ itest/nexus/nexus.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+imageName=nexus
+dockerMachineIp=$(docker-machine ip)
+
+url=https://$dockerMachineIp:8443/
+
+isRunning=$(docker ps | grep $imageName >/dev/null 2>&1 && echo true || echo false)
+if [ $isRunning = "false" ]; then
+
+  isCreated=$(docker ps -a | grep $imageName >/dev/null 2>&1 && echo true || echo false)
+  if [ $isCreated = "true" ]; then
+    docker start $imageName
+  else
+
+    if [ ! -f "$DIR/keystore.jks" ]; then
+      keytool -genkey -noprompt \
+        -keyalg RSA \
+        -keysize 2048 \
+        -validity 365 \
+        -alias nexus \
+        -dname "CN=$dockerMachineIp, OU=IT, O=Somewhere, L=Dallas, ST=TX, C=US" \
+        -keystore $DIR/keystore.jks \
+        -storepass changeit \
+        -keypass changeit
+    fi
+
+    docker run -d \
+      --name nexus \
+      -p 8081:8081 \
+      -p 8443:8443 \
+      -v $DIR/keystore.jks:/nexus-data/keystore.jks \
+      -e JKS_PASSWORD=changeit \
+      clearent/nexus:3.2.0-01
+  fi
+else
+  echo "$imageName is already running..."
+fi
+
+until $(curl --output /dev/null --silent --head --fail --insecure $url); do
+    printf '.'
+    sleep 5
+done
+
+echo "$imageName running at: $url"
