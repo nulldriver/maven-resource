fd0ad63cb7c723b26b32d364a213ebe49e5a4ad1
patrickcrocker <patrick.crocker@gmail.com>
2017-05-22 22:32:45 -0500

    make the task a bit more generic

--- ci/generate-docker-tags.sh
+++ ci/generate-docker-tags.sh
@@ -4,5 +4,5 @@ set -e
 
 version=$(cat version/version)
 
-echo $version > generate-docker-tags-output/tag-alpine
-echo "$version-debian" > generate-docker-tags-output/tag-debian
+echo $version > task-output/tag-alpine
+echo "$version-debian" > task-output/tag-debian
