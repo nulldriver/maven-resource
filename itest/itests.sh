7e362bf8f24bb724aa9c5a4e0cf62389c8a191cf
patrickcrocker <patrick.crocker@gmail.com>
2018-08-19 12:12:46 -0500

    + it_can_return_only_given_version_if_already_the_latest_version_found

--- itest/itests.sh
+++ itest/itests.sh
@@ -16,21 +16,38 @@ readonly UNIQUE_SNAPSHOT_PATTERN="\-[0-9]{8}\.[0-9]{6}-[0-9]{1,}"
 
 it_can_return_an_empty_list_if_no_versions_available() {
 
-  json=$(check_a_snapshot_and_return_the_unique_version "com.example:$(gen_artifact_id):jar")
-  assert_equals "[]" "$json"
+  response=$(check_a_snapshot_and_return_the_unique_version "com.example:$(gen_artifact_id):jar")
+  assert_equals "[]" "$response"
 }
 
 it_can_return_current_version_if_no_version_requested() {
 
   local artifact="com.example:$(gen_artifact_id):jar"
 
-  json=$(put_a_snapshot_and_return_the_unique_version "$artifact")
-  version_put=$(echo $json | jq -r '.version.version')
-  assert_matches "$version_put" "$UNIQUE_SNAPSHOT_PATTERN"
+  version=$(put_a_snapshot_and_return_the_unique_version "$artifact" | jq -r '.version.version')
+  assert_matches "$version" "$UNIQUE_SNAPSHOT_PATTERN"
 
   check_a_snapshot_and_return_the_unique_version "$artifact" | \
   jq -e \
-  --arg version "$version_put" \
+  --arg version "$version" \
+  '
+    . == [{version: $version}]
+  '
+}
+
+it_can_return_only_given_version_if_already_the_latest_version_found() {
+
+  local artifact="com.example:$(gen_artifact_id):jar"
+
+  put_a_snapshot_and_return_the_unique_version "$artifact"
+  put_a_snapshot_and_return_the_unique_version "$artifact"
+
+  version=$(put_a_snapshot_and_return_the_unique_version "$artifact" | jq -r '.version.version')
+  assert_matches "$version" "$UNIQUE_SNAPSHOT_PATTERN"
+
+  check_a_snapshot_and_return_the_unique_version "$artifact" | \
+  jq -e \
+  --arg version "$version" \
   '
     . == [{version: $version}]
   '
@@ -39,3 +56,4 @@ it_can_return_current_version_if_no_version_requested() {
 init_integration_tests
 run it_can_return_an_empty_list_if_no_versions_available
 run it_can_return_current_version_if_no_version_requested
+run it_can_return_only_given_version_if_already_the_latest_version_found
