0dc864fd96431e61285534d977e8f3bd42c6c3bb
Jacob Barrett <jabarrett@vmware.com>
2021-02-24 22:59:47 -0800

    Replaces xmllint with xqilla for XPath 2. Adds version filter.

--- test/check.sh
+++ test/check.sh
@@ -95,7 +95,33 @@ it_can_check_latest_from_three_versions() {
   '
 }
 
+it_can_check_filtered_version_from_four_versions() {
+
+  local src=$(mktemp -d $TMPDIR/check-src.XXXXXX)
+
+  local repository=$src/remote-repository
+  mkdir -p $repository
+
+  local url=file://$repository
+  local artifact=ci.concourse.maven:maven-resource:jar:standalone
+
+  local version1=$(deploy_artifact $url $artifact '1.0.0-rc.1' $src)
+  local version2=$(deploy_artifact $url $artifact '1.0.0-rc.2' $src)
+  local version3=$(deploy_artifact $url $artifact '2.0.0-rc.1' $src)
+  local version4=$(deploy_artifact $url $artifact '21.0.0-rc.1' $src)
+
+  check_artifact_filtered $url $artifact 'latest' $src '1.0.0-.*' | \
+  jq -e \
+  --arg version $version2 \
+  '
+    . == [
+      {version: $version}
+    ]
+  '
+}
+
 run it_can_check_from_one_version
 run it_can_check_from_three_versions
 run it_can_check_latest_from_one_version
 run it_can_check_latest_from_three_versions
+run it_can_check_filtered_version_from_four_versions
